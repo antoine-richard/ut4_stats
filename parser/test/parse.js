724144be63ab6dc89af4241605167a284e04a993
Antoine Richard <arichard@restlet.com>
2016-10-08 14:13:03 +0200

    removed useless console.log. added some semicolons.

--- parser/test/parse.js
+++ parser/test/parse.js
@@ -1,4 +1,3 @@
-
 var parse = require('../parse');
 var chai = require('chai');
 chai.use(require('chai-string'));
@@ -8,13 +7,11 @@ describe('parse', function() {
 
   it('should output CSV headers', function() {
     let result = parse("6:10 Kill: 4 1 30: JonathanMcCow killed Cyp' by UT_MOD_AK103").value();
-    console.log(result)
     expect(result).to.startsWith("killer,victim,weapon");
   });
 
   it('should extracts killer, victim and weapon', function() {
     let result = parse("6:10 Kill: 4 1 30: JonathanMcCow killed Cyp' by UT_MOD_AK103").value();
-    console.log(result)
     expect(result).to.have.entriesCount("JonathanMcCow,Cyp',UT_MOD_AK103", 1);
   });
 
