10614c4e38a2c1a4c5b86dc4c343333895f07171
Pierre Reliquet <preliquet@restlet.com>
2016-10-11 09:12:41 +0200

    feat(favorite gun): add the favorite gun chart

--- client/components.jsx
+++ client/components.jsx
@@ -1,16 +1,26 @@
-function refreshTable(stats, selector) {
+function objectToOrderedTable(object, selector) {
     ReactDOM.render(<table>
         <thead>
-            <th>Player</th>
-            <th>Count</th>
+            <tr>
+                <td>Player</td>
+                <td>Count</td>
+            </tr>
         </thead>
         <tbody>
-            {_.map(stats, player => {
-                return <tr>
-                    <td>{player.name}</td>
-                    <td>{player.count}</td>
-                </tr>
-            })}
+            {
+                _(object)
+                    .map((value, key) => {
+                        return { name: key, count: value };
+                    })
+                    .orderBy('count', 'desc')
+                    .map(stat => {
+                        return <tr key={stat.name}>
+                            <td>{stat.name}</td>
+                            <td>{stat.count}</td>
+                        </tr>
+                    })
+                    .value()
+            }
         </tbody>
     </table>, document.querySelector(selector))
 }
--- client/index.jsx
+++ client/index.jsx
@@ -1,46 +1,54 @@
 (function () {
 
     const WORLD = "<world>"
-    const SUICIDE_CAUSES = "MOD_FALLING";
-    const UT4_STATS = {
-        suicides: [],
-        clumsy: []
-    }
+    const UT4_STATS = {}
 
+    google.charts.load('current', { 'packages': ['corechart'] });
     fetchServerData()
-        .then(() => refreshTable(UT4_STATS.suicides, '#suicides'))
-        .then(() => refreshTable(UT4_STATS.clumsy, '#clumsy'));
+        .then(() => objectToOrderedTable(UT4_STATS.worstJumper, '#worstJumper'))
+        .then(() => objectToOrderedTable(UT4_STATS.hotPotato, '#hotPotato'))
+        .then(() => objectToOrderedTable(UT4_STATS.spawnIntruder, '#spawnIntruder'))
+        .then(() => objectToOrderedTable(UT4_STATS.worstSwimmer, '#worstSwimmer'))
+        .then(() => {
+            _.forEach(UT4_STATS.killDistributionByKillerAndWeapon, (distribution, killer) => {
+                distribution.unshift(['Weapon', 'Count']);
+                var data = google.visualization.arrayToDataTable(distribution);
+                var options = {
+                    title: `${killer}'s favorite gun`
+                };
+
+                var div = document.createElement('div');
+                div.id = killer;
+                document.querySelector('section.content').appendChild(div);
+
+                new google.visualization.PieChart(document.getElementById(killer)).draw(data, options);
+
+            });
+        });
+
 
     function fetchServerData() {
         let shouldContinue = true;
 
-        return fetch(`http://home/api/stats`)
+        return fetch(`http://89.90.86.195/api/stats`)
             .then(response => {
                 return response.json();
             })
             .then(numberOfKillByKillerVictimWeapon => {
-                UT4_STATS.suicides = _(numberOfKillByKillerVictimWeapon)
-                    .filter(data => data['_id'].killer === WORLD)
-                    .filter(data => SUICIDE_CAUSES == data['_id'].weapon)
-                    .map(data => {
-                        return {
-                            name: data['_id'].victim,
-                            count: data.count
-                        }
-                    })
-                    .orderBy('count', 'desc')
-                    .value()
+                UT4_STATS.worstJumper = numberOfKillByKillerVictimWeapon[WORLD].MOD_FALLING;
+                UT4_STATS.hotPotato = numberOfKillByKillerVictimWeapon[WORLD].UT_MOD_FLAG;
+                UT4_STATS.spawnIntruder = numberOfKillByKillerVictimWeapon[WORLD].UT_MOD_BERETTA;
+                UT4_STATS.worstSwimmer = numberOfKillByKillerVictimWeapon[WORLD].MOD_WATER;
 
-                UT4_STATS.clumsy = _(numberOfKillByKillerVictimWeapon)
-                    .filter(data => data['_id'].killer ===  data['_id'].victim)
-                    .map(data => {
-                        return {
-                            name: data['_id'].victim,
-                            count: data.count
-                        }
-                    })
-                    .orderBy('count', 'desc')
-                    .value()
+                UT4_STATS.killDistributionByKillerAndWeapon = _(numberOfKillByKillerVictimWeapon)
+                    .reduce((iterator, stats, killer) => {
+                        iterator[killer] = _(stats)
+                            .map((countByVictim, weapon) => {
+                                return [weapon, _(countByVictim).values().sum()]
+                            })
+                            .value();
+                        return iterator;
+                    }, {});
             })
             .catch(error => console.log(error));
     }
